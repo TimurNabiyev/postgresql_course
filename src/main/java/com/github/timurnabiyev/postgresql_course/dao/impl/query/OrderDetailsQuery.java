a350033a163c222209f8f75a424e9ada18f01b89
Timur Nabiev <tnabiev82@gmail.com>
2022-12-02 17:29:18 +0500

    add all lines to OrderDetailsQuery class

--- src/main/java/com/github/timurnabiyev/postgresql_course/dao/impl/query/OrderDetailsQuery.java
+++ src/main/java/com/github/timurnabiyev/postgresql_course/dao/impl/query/OrderDetailsQuery.java
@@ -22,6 +22,11 @@ public final class OrderDetailsQuery {
             "SELECT * FROM timur.order_details";
 
     public static final String UPDATE_ORDER_DETAIL_BY_ID = "" +
-            "";
+            "UPDATE timur.order_details SET customer_id, employee_id, order_date, required_date, shipped_date, " +
+            "ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country) " +
+            "WHERE order_id = ?";
+
+    public static final String DELETE_ORDER_DETAILS_BY_ID = "" +
+            "DELETE FROM timur.order_details WHERE order_id = ?";
 
 }
